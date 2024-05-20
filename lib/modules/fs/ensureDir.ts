682d2242484287bd3b5eabb2f1cc1e3de96ea94d
CamiloTDex <ctorres@dexfreight.io>
2024-05-19 19:21:41 -0500

    Fix typo

--- lib/modules/fs/ensureDir.ts
+++ lib/modules/fs/ensureDir.ts
@@ -2,5 +2,5 @@ import { exec } from "child_process";
 import { log } from "termx";
 
 export default async function ensureDir (path: string) {
-    await exec(`mkdir -p "${this.subPath(path)}"`);
+    await exec(`mkdir -p "${path}"`);
 }
